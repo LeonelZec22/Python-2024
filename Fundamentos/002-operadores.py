029d6e623fa003d03e796cb2f966aa61a1edea01
Leonel Zeceña <lazecena@bancoagricola.com.sv>
2024-08-04 16:16:23 -0600

    Finalizando la clase de operadores

---
+++ Fundamentos/002-operadores.py
@@ -0,0 +1,72 @@
+
+#Operadores de asignación
+
+numero = 5 
+
+#print(numero) #resultado 5
+
+numero += 10 
+
+#print (numero) #5 + 10 el resultado 15
+
+numero -=3  
+
+#print (numero) # 15 - 3 el resultado es 12
+
+numero *= 2
+
+#print (numero) #12 *2 el resultado 24
+
+numero /=3
+
+#print(numero) # 24 * 3 el resultado es 8.0
+
+numero %= 3
+
+#print(numero) # 8 % 3 lo que sobra es 2
+
+numero +=20 
+
+numero //= 2
+
+#print (numero) #22 / 2 el resultado es 11
+
+# Operadores Aritméticos
+
+numero1 = 5
+
+numero2 = 2
+
+"""
+print(numero1 + numero2) #-> 7
+print(numero1 - numero2) #-> 3
+print(numero1 * numero2) #-> 10
+print(numero1 / numero2) #-> 2.5
+print(numero1 % numero2) #-> 1
+print(numero1 // numero2) #-> 2
+print(numero1 ** numero2) #-> 25
+
+"""
+"""
+print("Hola " + "Soy Leonel")
+
+print (numero1 + numero2)
+
+print("Hola","Soy Leonel", 25)
+"""
+
+#Operadores comparación
+"""
+print ("¿5 es igual 5?", 5 == 5)
+print("¿5 no es igual a 7?", 5 != 7)
+print("¿5 es mayor que 7?", 5>7)
+print("¿5 es menor que 7?", 5 <7)
+print("¿5 es mayor o igual que 7?", 5>=7)
+print("¿5 es menor que 7?", 5 <=7)
+"""
+
+#Operadores Lógicos
+
+print("¿3 es menor que 5 y al mismo tiempo es menor que 10?", 3 < 5 and 3 < 10)
+print("¿3 es menor que 5 o 4 es mayor que 7?", 3 < 5 or 7 > 4)
+print("¿3 es menor que 5 y al mismo tiempo es menor que 10?", not(3 < 5 and 3 < 10))
