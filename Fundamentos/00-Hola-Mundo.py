dc44b605814e6e08cea2d96d84212bfa0631f2e6
Leonel Zeceña <lazecena@bancoagricola.com.sv>
2024-08-04 10:45:09 -0600

    Terminas la clase de introducción a Python

---
+++ Fundamentos/00-Hola-Mundo.py
@@ -0,0 +1,32 @@
+#Los comentarios empiezan con # o """"
+#Imprimiendo en consola el texto: "Hola Mundo"
+
+print("Hola Mundo desde Python")
+
+print('Esto es un texto con comillas simples')
+
+#Viendo los números
+print(2+2)
+
+print(2.5+2)
+
+print(2.999+2.6777)
+
+#Viendo los booleanos
+
+print('¿Está encendida la luz?')
+
+print(True)
+
+#Si la respuesta es si su valor debe de ser true
+#Si la respuesta es no su valor debe de ser false
+
+""" 
+    ¿Como saber que tipo de dato es esa información?
+"""
+
+print(type('Esto es una cadena de texto'))
+
+print(type(5))
+
+print(type(True))
